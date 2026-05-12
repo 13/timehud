b206e18e7f4d9e172a353d4aeaedf854e4e17711
13 <13@users.noreply.github.com>
2026-05-12 21:45:36 +0200

    Simplify AppImage download and installation process
    Removed unnecessary variables and simplified download and installation steps.

--- install-appimage.sh
+++ install-appimage.sh
@@ -25,13 +25,9 @@ ASSET_URL="$(curl -fsSL \
   | grep AppImage \
   | cut -d '"' -f 4)"
 
-FILE="$(basename "$ASSET_URL")"
+echo "Downloading..."
+curl -fsSL -o "$TMP_DIR/timehud.AppImage" "$ASSET_URL"
 
-echo "Downloading $FILE..."
-curl -fsSL -o "$TMP_DIR/$FILE" "$ASSET_URL"
-
-echo "Installing..."
-mv "$TMP_DIR/$FILE" "$INSTALL_DIR/timehud"
-chmod +x "$INSTALL_DIR/timehud"
+install -m 755 "$TMP_DIR/timehud.AppImage" "$INSTALL_DIR/timehud"
 
 echo "Done! Run: timehud"
